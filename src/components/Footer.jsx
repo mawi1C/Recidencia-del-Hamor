16cd01f072da12ae62900b6cc14d5c9b6d89a365
mawi1C <mawie1C@gmail.com>
2025-03-18 18:04:13 +0800

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -108,7 +108,7 @@ const Footer = () => {
       <div className="bg-gray-950 py-6">
         <div className="max-w-7xl mx-auto px-4 flex flex-col md:flex-row justify-between items-center">
           <p className="text-gray-500 text-sm">
-            &copy; {currentYear} Residencia del Hamor. All rights reserved.
+            &copy; {currentYear} Residencia del Hamor. - Den John Emmanuel Cabria
           </p>
           <div className="flex space-x-6 mt-4 md:mt-0">
             <Link to="/privacy-policy" className="text-gray-500 hover:text-gray-300 text-sm transition duration-300">
